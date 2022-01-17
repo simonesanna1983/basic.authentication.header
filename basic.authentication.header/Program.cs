7e3c49c0454301426b48cb071a69d024b92d3bd2
Sanna <simone.sanna1983@gmail.com>
2022-01-17 19:16:00 +0100

    Add project files.

---
+++ basic.authentication.header/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace basic.authentication.header
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username:");
+            string userName = Console.ReadLine();
+
+
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+
+            string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(userName + ":" + password));
+
+            Console.WriteLine();
+            Console.WriteLine($"Basic {svcCredentials}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadLine();
+        }
+    }
+}
